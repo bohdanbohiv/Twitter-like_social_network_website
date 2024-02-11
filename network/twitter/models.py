75a275f8e36c4700cc44fb6d72997e66e47f0403
Bohdan <bohiv.bohdan@gmail.com>
2024-02-11 23:10:18 +0100

    Added registration for users

--- network/twitter/models.py
+++ network/twitter/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    followings = models.ManyToManyField('self', related_name='followers', symmetrical=False, blank=True)
--- network/twitter/views.py
+++ network/twitter/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.db import IntegrityError
+from django.shortcuts import redirect, render
+
+from .models import User
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'twitter/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmation = request.POST['confirmation']
+        if password != confirmation:
+            return render(request, 'twitter/register.html',
+                          {'message': 'Passwords must match.'})
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, 'twitter/register.html',
+                        {'message': 'Username already taken.'})
+        login(request, user)
+        return redirect('index')
+    return render(request, 'twitter/register.html')
